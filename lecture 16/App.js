90f90f92b431bb2f235f6714659cd28b248c7fc1
Sagar-Admane <118822902+Sagar-Admane@users.noreply.github.com>
2024-06-11 20:31:43 +0530

    Add files via upload

---
+++ lecture 16/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const NAME = 'cipherschools'
+  return (
+    <>
+    <div>
+      <h1 style={{
+        color : "red",
+        textAlign: "center"
+      }}>Hello from {NAME}!</h1>
+    </div>
+    <div>
+      <p>This is CipherSchools</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
