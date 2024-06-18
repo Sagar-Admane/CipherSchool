17f51493e3eea3fc85c7c5abc03eb392d7c822e4
Sagar-Admane <118822902+Sagar-Admane@users.noreply.github.com>
2024-06-18 23:50:44 +0530

    Add files via upload

---
+++ lecture 20/screens/ToDoScreen.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Task from "../component/Task";
+
+const ToDoScreen = () => {
+    const [taskList, setTaskList] = useState([]);
+
+    return (
+        <div className="screen">
+            <h1 className="ui heading center">To Do List</h1>
+            <div>
+                <button onClick={(e)=>{
+                    setTaskList([...taskList, {
+                        title: "Go to gym",
+                        description : "Going to gym is good for muscle growth",
+                    },
+                ]);
+                }}
+                className="ui secondary button">Add Task</button>
+                {taskList.map((task) => (
+                    <Task />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default ToDoScreen;
