c24bb332005cfda9c9f7c1f0cdf7fe9894a47b40
Sagar-Admane <118822902+Sagar-Admane@users.noreply.github.com>
2024-07-03 21:47:42 +0530

    Add files via upload

---
+++ lecture 26/App.js
@@ -0,0 +1,27 @@
+import { useContext, useState } from "react";
+import AddTask from "./component/AddTask";
+import ToDoScreen from "./screens/ToDoScreen";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {TaskProvider} from "./context/TaskContext";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <ToDoScreen />,
+    },
+    {
+        path: "/add-task",
+        element: <AddTask />
+    },
+])
+
+const App = () => {
+   
+    return(
+        <TaskProvider>
+            <RouterProvider router={router} />
+        </TaskProvider>
+    )
+}
+
+export default App;
